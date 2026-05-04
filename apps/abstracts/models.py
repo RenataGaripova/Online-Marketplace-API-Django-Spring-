2858fc88fc98c5e36ffaf37e4f32fea8dbbb3cce
Renata Garipova <rena.garipova.2005@gmail.com>
2026-05-04 09:40:34 +0500

    Changed soft deletable model

--- apps/abstracts/models.py
+++ apps/abstracts/models.py
@@ -1,5 +1,5 @@
 # Python modules
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 # Django modules
 from django.db.models import (
@@ -8,15 +8,36 @@ from django.db.models import (
     QuerySet,
     Manager,
 )
+from django.db.utils import NotSupportedError
 from django.utils import timezone as django_timezone
 
 
-class SoftDeleteManager(Manager):
-    """Manager that excludes soft-deleted objects."""
+T = TypeVar("T", bound=Model)
 
-    def get_queryset(self) -> QuerySet:
-        """Return queryset excluding soft-deleted objects."""
-        return super().get_queryset().filter(deleted_at__isnull=True)
+
+class SoftDeletableQuerySet(QuerySet, Generic[T]):
+    """Soft deletable queryset."""
+
+    def _raise_not_supported_error(self, message: str) -> None:
+        """Raises a not supported error that doesn't let making a query,"""
+        raise NotSupportedError(message)
+
+    def delete(self) -> tuple[int, dict[str, int]]:
+        """Soft-deletes a queryset ob objects,"""
+        count: int = self.update(deleted_at=django_timezone.now())
+        return count, {self.model._meta.label: count}
+
+    def hard_delete(self) -> int:
+        """Deletes a queryset of objects from the DB permanently."""
+        return super().delete()
+
+    def get_soft_deleted(self) -> "SoftDeletableQuerySet[T]":
+        """Returns soft deleted objects of a Model."""
+        return self.filter(deleted_at__isnull=False)
+
+    def get_not_deleted(self) -> "SoftDeletableQuerySet[T]":
+        """Returns objects of a Model, excluding soft deleted intances."""
+        return self.filter(deleted_at__isnull=True)
 
 
 class AbstractBaseModel(Model):
@@ -31,17 +52,27 @@ class AbstractBaseModel(Model):
     deleted_at: DateTimeField = DateTimeField(
         null=True, blank=True, verbose_name="Deleted at"
     )
-    objects: Manager = SoftDeleteManager()
-    all_objects: Manager = Manager()
+    objects: Manager = SoftDeletableQuerySet.as_manager()
 
     class Meta:
         """Meta class."""
 
         abstract = True
 
-    def soft_delete(
-        self, *args: tuple[Any, ...], **kwargs: dict[Any, Any]
-    ) -> None:
+    @property
+    def is_deleted(self):
+        return self.deleted_at is not None
+
+    def delete(self, *args: tuple[Any, ...], **kwargs: dict[Any, Any]) -> None:
         """Soft delete the model's object."""
         self.deleted_at = django_timezone.now()
         self.save(update_fields=["deleted_at"])
+
+    def hard_delete(
+        self, using: Any = None, keep_parents: bool = False
+    ) -> None:
+        """Delete an instance from the DB."""
+        super().delete(
+            using=using,
+            keep_parents=keep_parents,
+        )
